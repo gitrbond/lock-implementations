ac6a646a274a560130cd05ec8fd73f8e01afcf55
gitrbond <bond2000r@mail.ru>
2023-06-05 20:35:28 +0300

    clean code

--- src/test/java/ru/sbt/mipt/locks/CountingTest.java
+++ src/test/java/ru/sbt/mipt/locks/CountingTest.java
@@ -1,8 +1,12 @@
 package ru.sbt.mipt.locks;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import ru.sbt.mipt.locks.impl.TASLock;
+import ru.sbt.mipt.locks.impl.*;
+import ru.sbt.mipt.locks.util.LockTypes;
+
+import java.util.List;
 
 import static ru.sbt.mipt.locks.util.SystemPropertyParser.parseBenchmarkOptions;
 
@@ -25,72 +29,53 @@ public class CountingTest {
         options = parseBenchmarkOptions(defaultOptions);
     }
 
-    private void benchmarkAndTest(SpinLock lock) throws InterruptedException {
+    private void benchmarkAndTest(SpinLock lock) {
         SimpleCounter counter = new SimpleCounter(0, lock);
-//        ExecutorService executorService = newFixedThreadPool(options.nThreads());
-//        ParallelCountTaskExecutor taskExecutor = new ParallelCountTaskExecutor(executorService);
 
-//        long expectedCount = 0;
-//        List<CounterIncrementOperation> operations;
+        try {
+            long avgThroughput = 0;
 
-        // warmup
-        long avgThroughput = 0;
-        for (int warmupIter = 1; warmupIter <= options.warmupIterations(); warmupIter++) {
-            avgThroughput += executeIterationAndReturnPerformance(counter, options.warmupMillisecs());
+            // warmup
+            for (int warmupIter = 1; warmupIter <= options.warmupIterations(); warmupIter++) {
+                avgThroughput += getOperationPerSec(counter, options.warmupMillisecs());
+            }
+
+            // measure
+            avgThroughput = 0;
+            for (int measureIter = 1; measureIter <= options.measureIterations(); measureIter++) {
+                avgThroughput += getOperationPerSec(counter, options.measureMillisecs());
+            }
+
+            avgThroughput /= options.measureIterations();
+
+            System.out.println("$");
+            System.out.println("Benchmark results for " + lock.getClass().getSimpleName() + ":");
+            System.out.println(options);
+            System.out.println("avgThroughput = " + avgThroughput + " op/sec");
+        } catch (InterruptedException e) {
+            System.out.println("failed to benchmark " + lock.getClass().getSimpleName() + " due to error: " + e.getMessage());
         }
-
-        // measure
-        avgThroughput = 0;
-        for (int measureIter = 1; measureIter <= options.measureIterations(); measureIter++) {
-            avgThroughput += executeIterationAndReturnPerformance(counter, options.measureMillisecs());
-        }
-        avgThroughput /= options.measureIterations();
-
-        System.out.println("$");
-        System.out.println("Benchmark results for " + lock.getClass().getSimpleName() + ":");
-        System.out.println(options);
-        System.out.println("avgThroughput = " + avgThroughput + " op/sec");
-//        System.out.println("avgTime to execute one task = " +
-//                avgThroughput * 1_000_000 * options.nThreads() / options.nMeasureTotalTasks() + "ns");
     }
 
-    public long executeIterationAndReturnPerformance(SimpleCounter counter, long testTimeMillis) throws InterruptedException {
-//        List<Thread> threads = new ArrayList<>();
-        // lock the lock so threads don't start immediately
-//        lock.lock();
-//        SimpleCounter counter = new SimpleCounter(0, lock);
-
-//        volatile int cnt;
-
-//        ThreadLocal<Boolean> finished = new ArrayList<new Boolean(false)>();
-//        CounterIncrementOperation couter = new CounterIncrementOperation(lock);
+    public long getOperationPerSec(SimpleCounter counter, long testTimeMillis) throws InterruptedException {
         counter.getLock().lock();
 
         for (int i = 0; i < options.nThreads(); i++) {
             int threadInd = i;
-            Thread t = new Thread(() -> {
-                    threadRunner(counter, threadInd);
-            });
+            Thread t = new Thread(() -> threadRunner(counter, threadInd));
 
-//            threads.add(t);
             t.start();
         }
 
         // sleep to wait until all threads start
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        finally {
-            counter.getLock().unlock();
-        }
+        Thread.sleep(500);
+        counter.getLock().unlock();
         // all the threads start counting
 
         Thread.sleep(testTimeMillis); // 5 sec
         long resultCount = counter.getCount();
         System.out.println("counter = " + resultCount);
-        Thread.sleep(15_000); // 5 sec
+        Thread.sleep(15_000); // 15 sec
         return resultCount * 1000 / testTimeMillis; // op/sec
     }
 
@@ -106,63 +91,43 @@ public class CountingTest {
     }
 
     @Test
-    public void TASSimpleLockTest() throws InterruptedException {
+    public void TASLockTest() {
         benchmarkAndTest(new TASLock());
     }
 
-//    @Test
-//    public void MCSSimpleLockTest() throws InterruptedException {
-//        executeIteration(new MCSLock());
-//    }
-//
-//    @Test
-//    public void CLHSimpleLockTest() throws InterruptedException {
-//        executeIteration(new CLHLock());
-//    }
-//
-//    @Test
-//    public void BackoffSimpleLockTest() throws InterruptedException {
-//        executeIteration(new BackoffLock());
-//    }
-//
-//    @Test
-//    public void TASLockTest() {
-//        benchmarkAndTest(new TASLock());
-//    }
+    @Test
+    public void TTASLockTest() {
+        benchmarkAndTest(new TTASLock());
+    }
 
-//    @Test
-//    public void TTASLockTest() {
-//        benchmarkAndTest(new TTASLock());
-//    }
-//
-//    @Test
-//    public void BackoffLockTest() {
-//        benchmarkAndTest(new BackoffLock());
-//    }
-//
-//    @Test
-//    public void CLHLockTest() {
-//        benchmarkAndTest(new CLHLock());
-//    }
-//
-//    @Test
-//    public void MCSLockTest() {
-//        benchmarkAndTest(new MCSLock());
-//    }
-//
-//    // Фиксация некорректной параллельной обработки без использования синхронизации.
-//    // Тест должен падать при 1 потоке или если totalOperations достаточно мало (~единицы тысяч).
-//    @Disabled
-//    @Test
-//    public void NoLockTest() {
-//        benchmarkAndTest(new NoLock());
-//    }
-//
-//    @Disabled
-//    @Test
-//    public void AllLocksTest() {
-//        List<SpinLock> locks = LockTypes.LOCK_LIST;
-//        locks.forEach(this::benchmarkAndTest);
-//    }
+    @Test
+    public void BackoffLockTest() {
+        benchmarkAndTest(new BackoffLock());
+    }
+
+    @Test
+    public void CLHLockTest() {
+        benchmarkAndTest(new CLHLock());
+    }
+
+    @Test
+    public void MCSLockTest() {
+        benchmarkAndTest(new MCSLock());
+    }
+
+    // Фиксация некорректной параллельной обработки без использования синхронизации.
+    // Тест должен падать при 1 потоке или если totalOperations достаточно мало (~единицы тысяч).
+    @Disabled
+    @Test
+    public void NoLockTest() {
+        benchmarkAndTest(new NoLock());
+    }
+
+    @Disabled
+    @Test
+    public void AllLocksTest() {
+        List<SpinLock> locks = LockTypes.LOCK_LIST;
+        locks.forEach(this::benchmarkAndTest);
+    }
 
 }
